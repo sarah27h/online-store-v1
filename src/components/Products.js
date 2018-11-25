95d9233efa6e18e7fb575c9351881fd8011b1fa1
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-25 17:20:31 +0200

    Feat: import PRODUCTS in <Products />

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,8 +2,18 @@ import React, {Component} from 'react';
 import Filters from './Filters.js';
 import ProductTable from './ProductTable.js';
 import ProductForm from './ProductForm.js';
+import {PRODUCTS} from '../ProductsData.js'
 
 class Products extends Component {
+
+    state= {
+        products: []
+    }
+
+    componentDidMount() {
+        this.setState({ products : PRODUCTS })
+    }
+
     render() {
         return(
             <div>
