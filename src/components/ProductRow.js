3e1992011b64a833f7f0c6d72a7f5dce9e81e08b
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-30 18:16:10 +0200

    Feat: create <ProductTableHeader /> <ProductRow />

---
+++ src/components/ProductRow.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ProductRow extends Component {
+    render() {
+        return(
+            <tr></tr>
+        );
+    }
+}
+
+
+export default ProductRow;
---
+++ src/components/ProductTableHeader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ProductTableHeader extends Component {
+    render() {
+        return(
+            <th></th>
+        );
+    }
+}
+
+
+export default ProductTableHeader;
