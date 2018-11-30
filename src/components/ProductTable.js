aefaab95ea94dfcc56a8929f5285d492061d8dfc
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-30 18:27:02 +0200

    Feat: build <ProductTable />

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
+import ProductTableHeader from './ProductTableHeader';
+import ProductRow from './ProductRow';
 
 class ProductTable extends Component {
+
     render() {
+        // convert products object to an array to be able to iterate over its items
+        const productsAsArray = Object.keys(this.props.products).map((key) => this.props.products[key]);
+        console.log(productsAsArray);
+        // create products table
+        const tableRows = productsAsArray.map((product) => {
+            return(
+                <ProductRow product={product} key={product.id}/>
+            );
+        });
         return(
             <div>
-                
+                <table>
+                    {/* table header */}
+                    <thead>
+                        <tr>
+                            <ProductTableHeader />
+                        </tr>
+                    </thead>
+
+                    {/* table rows */}
+                    <tbody>
+                        {tableRows}
+                    </tbody>
+                </table>
+
             </div>
         );
     }
