0898040a4d8a0a9bc2c0b4946247bf4778f8aa4e
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-30 19:47:07 +0200

    Styles: build, add styles

--- src/components/ProductTableHeader.js
+++ src/components/ProductTableHeader.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import '../styles/ProductTableHeader.css'
 
 class ProductTableHeader extends Component {
     render() {
         return(
-            <th></th>
+            <th>
+                {this.props.columnName}
+                <button className="sort-btn btn-current-used"> &#x25B2;</button>
+                <button className="sort-btn"> &#x25BC;</button>
+            </th>
         );
     }
 }
