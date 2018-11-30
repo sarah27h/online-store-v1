06f2079082a324a5913c41d76910524f2c124b91
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-30 19:48:18 +0200

    Styles: build, style <ProductRow />

--- src/components/ProductRow.js
+++ src/components/ProductRow.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import '../styles/ProductRow.css'
 
 class ProductRow extends Component {
     render() {
         return(
-            <tr></tr>
+            <tr>
+                <td>
+                    {this.props.product.name}
+                </td>
+                <td>
+                    {this.props.product.price}
+                </td>
+                <td>
+                    <button className="remove-btn">X</button>
+                </td>
+            </tr>
         );
     }
 }
