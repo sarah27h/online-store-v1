1ff0bb1fe6d7bdafa4d6d92f5fc095b8aa6a4cf4
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-30 18:19:13 +0200

    Feat: build <Filters />

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import '../styles/Filters.css'
 
 class Filters extends Component {
     render() {
         return(
-            <div>
-                
-            </div>
+            <section className="filter-container">
+                <form>
+                <div className="form-row">
+                    <input className="form-text" type="text" placeholder="Search..."/>
+                </div>
+                <div className="form-row">
+                    <input className="form-check" type="checkbox" id="show"/>                    
+                    <label htmlFor="show"> Only show products in stock</label>
+                </div>
+                </form>
+            </section>
         );
     }
 }
