15a57f1884d1b81fb8149419eda04f7d8a8e2751
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-12-01 19:11:50 +0200

    Feat: bound filter inputs to props values

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -7,10 +7,18 @@ class Filters extends Component {
             <section className="filter-container">
                 <form>
                 <div className="form-row">
-                    <input className="form-text" type="text" placeholder="Search..."/>
+                    <input 
+                        className="form-text" 
+                        type="text" 
+                        placeholder="Search..."
+                        value={this.props.filterText}/>
                 </div>
                 <div className="form-row">
-                    <input className="form-check" type="checkbox" id="show"/>                    
+                    <input 
+                        className="form-check" 
+                        type="checkbox" 
+                        id="show"
+                        checked={this.props.inStock}/>                    
                     <label htmlFor="show"> Only show products in stock</label>
                 </div>
                 </form>
