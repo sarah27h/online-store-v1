99b6091871f1c67b6311fd59afd8f7a47d2c6495
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-12-05 17:34:27 +0200

    Feat: mark the currently selected sort button with the CSS

--- src/components/ProductTableHeader.js
+++ src/components/ProductTableHeader.js
@@ -3,11 +3,23 @@ import '../styles/ProductTableHeader.css'
 
 class ProductTableHeader extends Component {
     render() {
+
+        // mark the currently selected sort button with the CSS
+        // detect current used to sort products
+        // for btn1 (asc)
+        // check this.props.sort.direction === 'asc' this.props.columnName === 'price or name'
+        // then if true -> className = 'sort-btn btn-current-used'
+        //      if false -> className = 'sort-btn'
+        let ascCurrentSort = this.props.sort.direction === 'asc' && this.props.columnName === this.props.sort.columnName? 'sort-btn btn-current-used' : 'sort-btn';
+        let descCurrentSort = this.props.sort.direction === 'desc' && this.props.columnName === this.props.sort.columnName? 'sort-btn btn-current-used' : 'sort-btn';
+
         return(
             <th>
                 {this.props.columnName}
-                <button className="sort-btn btn-current-used"> &#x25B2;</button>
-                <button className="sort-btn"> &#x25BC;</button>
+
+                <button className={ascCurrentSort}> &#x25B2;</button>
+
+                <button className={descCurrentSort}> &#x25BC;</button>
             </th>
         );
     }
