b31171e207fd2eab4ca44fe772ff2d1370d39ff4
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-12-06 17:24:28 +0200

    Refactor: change input names to be the same as keys products

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -10,27 +10,27 @@ class ProductForm extends Component {
                         <label htmlFor="name">Name</label>
                         </div>
                     <div className="form-input">
-                        <input className="form-text" id="name" type="text" name="product-name" placeholder="product name" />
+                        <input className="form-text" id="name" type="text" name="name" placeholder="product name" />
                     </div>
                     <div className="form-row">
                         <label htmlFor="category">Category</label>
                         </div>
                     <div className="form-input">
-                        <input className="form-text" id="category" type="text" name="product-category" placeholder="product category" />
+                        <input className="form-text" id="category" type="text" name="category" placeholder="product category" />
                     </div>
                     <div className="form-row">
                         <label htmlFor="price">Price</label>
                         </div>
                     <div className="form-input">
-                        <input className="form-text" id="price" type="text" name="product-price" placeholder="product price" />
+                        <input className="form-text" id="price" type="text" name="price" placeholder="product price" />
                     </div>
                     <div className="form-row">
-                        <input className="form-check" type="checkbox" id="show"/>                    
+                        <input className="form-check" type="checkbox" name="inStock" id="show"/>
                         <label htmlFor="show"> In stock?</label>
                     </div>
                     <div className="form-row">
                         <button className="sub-btn" type="submit">Save</button>
-                    </div>                    
+                    </div>
                 </form>
             </div>
         );
